3270ec88ddade21e275f835696438b9b3a39dd89
Steich <s0660494@tu-dresden.de>
2015-07-08 16:15:32 +0200

    getter und setter Funktionen für statusNXT und aktuelle angefahrene Arbeitsstation

--- components/ComInterface/src/de/ees/group1/cont/ControlStation.java
+++ components/ComInterface/src/de/ees/group1/cont/ControlStation.java
@@ -10,6 +10,14 @@ public class ControlStation {
 	private ProductionOrder currentOrder;
 	private ProductionStep currentStep;
 	private int currentStepNumber;
+	/*
+	 * Gibt an ob der NXT die Arbeitsstation anfährt oder an ihr vorbeifährt true: fährt an; false: fährt weiter
+	 */
+	private boolean statusNXT;
+	/*
+	 * gibt die aktuelle Arbeitsstation an die der NXT anfragt
+	 */
+	private WorkStation workstation;
 	
 	/*
 	 * übergibt den gerade an den NXT übermittleten Auftrag an die ControlStation
@@ -30,8 +38,27 @@ public class ControlStation {
 		return currentStep;
 	}
 	
+	public void setStatusNXT(boolean statusNXT){
+		this.statusNXT=statusNXT;
+	}
+	
+	public void setCurrentWorkstation(WorkStation workstation){
+		this.workstation=workstation;
+	}
+	
+	public WorkStation getCurrentWorkstation(){
+		return workstation;
+	}
+	
+	public boolean getStatusNXT(){
+		return statusNXT;
+	}
+	
+		
+}
+	
 	
 	
 	
 
-}
+
