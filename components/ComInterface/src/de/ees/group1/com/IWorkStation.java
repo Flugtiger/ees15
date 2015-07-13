0655642c15f310be4d2549dec79612d5b79e9583
Steich <s0660494@tu-dresden.de>
2015-07-13 13:39:10 +0200

    updated interface IWorkStation

--- components/ComInterface/src/de/ees/group1/com/IWorkStation.java
+++ components/ComInterface/src/de/ees/group1/com/IWorkStation.java
@@ -1,7 +1,11 @@
 package de.ees.group1.com;
 
+import de.ees.group1.model.*;
 public interface IWorkStation {
 	/*
-	 * TODO: still to be specified
+	 * Methode in der die Arbeitsstation dem NXT mitteilt, ob er an die Arbeitsstation ranfahren darf oder 
+	 * weiterfahren muss, ihr muss die aktuelle Arbeitsstation mitgeteilt werden
 	 */
+	void enterWorkstation(ProductionStep currentStep);
+	void simulateWork(Boolean k);
 }
