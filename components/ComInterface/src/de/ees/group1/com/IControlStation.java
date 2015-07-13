ec64a964979e75ef3f97b52d16cf4a8fb5f5554b
Steich <s0660494@tu-dresden.de>
2015-07-13 13:38:34 +0200

    updated interface

--- components/ComInterface/src/de/ees/group1/com/IControlStation.java
+++ components/ComInterface/src/de/ees/group1/com/IControlStation.java
@@ -1,10 +1,17 @@
 package de.ees.group1.com;
 
+import de.ees.group1.model.ProductionOrder;
+import de.ees.group1.model.ProductionStep;
+
 public interface IControlStation {
 	
 	void reachedParkingPositionInd(int orderID ,int nextWorkingStep);
 
-	/*
-	 * TODO: more still to be specified
-	 */
+	ProductionOrder sendProductionOrder();
+	void setCurrentStep(ProductionStep step);
+	ProductionStep getCurrentStep();
+	boolean waitingPosition(int status);
+	void finishedWorkingStep(int finish);
+	void startWorkingStep(boolean start);
+	void enterWorkingStation(int status);
 }
